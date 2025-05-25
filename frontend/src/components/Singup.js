6474282030e4e0cf33ac246552adbd53d3bce09a
Ayan Manna <ayanmanna858@gmail.com>
2025-05-25 09:01:01 +0530

    new

--- frontend/src/components/Singup.js
+++ frontend/src/components/Singup.js
@@ -22,7 +22,7 @@ const Signup = () => {
       return;
     }
 
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("https://my-nootbook.vercel.app/api/auth/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
